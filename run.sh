427e8a56f38d24cc8186ff679d8c46ffd068adfe
sieverett <silaseverett@gmail.com>
2024-09-11 17:25:57 -0700

    run.sh added

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Make sure setup.sh is executable
-chmod +x setup.sh
-
-# This script installs wkhtmltopdf
-./setup.sh
+sudo apt update
+sudo apt install -y wkhtmltopdf
 
 python -m streamlit run quantiq.py --server.port 8000 --server.address 0.0.0.0
